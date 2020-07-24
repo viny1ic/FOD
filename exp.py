252560e4c7e774b57972f6c9029c9c899af8986e
Sagnik Sarkar <sagnik106@gmail.com>
2020-07-24 23:34:55 +0530

    Time complexity reduction

--- exp.py
+++ exp.py
@@ -3,11 +3,11 @@ import numpy as np
 import time
 from playsound import playsound
 
-url = "http://192.168.2.2:8080" # Your url might be different, check the app
-key = cv2. waitKey(1)
-webcam = cv2.VideoCapture(url+"/video")
+#url = "http://192.168.2.2:8080" # Your url might be different, check the app
+#key = cv2. waitKey(1)
+webcam = cv2.VideoCapture(0)
 # webcam = cv2.VideoCapture(0)
-webcam.set(cv2.CAP_PROP_BUFFERSIZE,1)
+#webcam.set(cv2.CAP_PROP_BUFFERSIZE,1)
 
 fpsLimit = 0.5 # throttle limit
 
@@ -19,26 +19,12 @@ def detect(img1, img2):
     blur2 = cv2.medianBlur(img2,15)
     img1_gray = cv2.cvtColor(blur1, cv2.COLOR_BGR2GRAY)
     img2_gray = cv2.cvtColor(blur2, cv2.COLOR_BGR2GRAY)
-    diff = cv2.absdiff(blur1, blur2)
+    diff = cv2.absdiff(img1_gray, img2_gray)
     # mask = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+    imask =  diff>100
+    print(np.sum(np.reshape(imask,(-1))))
 
-    th = 35
-    imask =  diff>th
-
-    canvas = np.zeros_like(img2, np.uint8)
-    canvas[imask] = img2[imask]
-
-    cv2.imwrite("result.jpeg", canvas)
-    result = cv2.imread("result.jpeg")
-    a = np.asarray(result)
-
-    ans2 = np.where(a>100)
-    print(len(a[ans2]))
-
-    if len(a[ans2])>1000:
-        return True
-    else:
-        return False
+    return np.sum(np.reshape(imask,(-1)))>1000
 
 def grab(n):
         ret, img = webcam.read()
@@ -62,7 +48,7 @@ while True:
         img1 = cv2.imread("img_"+str(i)+".jpg")
         img2 = cv2.imread("img_"+str(i-1)+".jpg")
         if detect(img1, img2)==True:
-            print("Suspicious")
+            #print("Suspicious")
             reference=i-1
             imgr = cv2.imread("img_"+str(reference)+".jpg")
             i+=1
@@ -70,13 +56,13 @@ while True:
                 grab(i)
                 img2 = cv2.imread("img_"+str(i)+".jpg")
                 if detect(imgr, img2)==False:
-                    print("Not detected")
+                    #print("Not detected")
                     break
                 i+=1
             if j==6:
-                print("confirmed")
+                #print("confirmed")
                 playsound("salamisound-4208277-smoke-detector-3-x-beeps.mp3")
                 break
         i+=1
         startTime = time.time()
-        print(time.time()-iterStart)
+        #print(time.time()-iterStart)
